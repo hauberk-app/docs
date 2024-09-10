326d685b46782604013125e117e7e2efa463f90c
Dane Stevens <12450895+dane-stevens@users.noreply.github.com>
2024-09-10 08:50:47 -0400

    Update typescript.ts

--- examples/emails/typescript.ts
+++ examples/emails/typescript.ts
@@ -4,14 +4,7 @@ const hauberk = new Hauberk('hk_123456789');
 // Checks for .env HAUBERK_API_KEY by default
 
 (async function() {
-  /**
-   * @returns { 
-   *   success: boolean, 
-   *   data?: { messageId: string; },
-   *   error?: { code: string; message: string; },
-   *   statusCode: number
-   * } 
-   */
+
   const { success, data, error } = await hauberk.emails.send({
     from: 'hello@yourapp.com',
     to: ['customer@example.com'],
